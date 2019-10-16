7d95ec64e91d507ca208ae9e547ee2afe2c952e6
johnmcl001 <john.mc-loughlin.3@ucdconnect.ie>
2019-10-16 17:14:20 +0100

    Circularly linked list

---
+++ CircularlyLinkedList/src/CircularlyLinkedListTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircularlyLinkedListTest {
+
+  @org.junit.jupiter.api.Test
+  void getSize() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void isEmpty() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void first() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void last() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void addFirst() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void addLast() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void removeFirst() {
+  }
+}
