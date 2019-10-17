3eb174700776b40f8edfed16ddbcfcf36a548d30
johnmcl001 <john.mc-loughlin.3@ucdconnect.ie>
2019-10-17 18:24:57 +0100

    Stack

---
+++ Stacks/src/ArrayStackTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayStackTest {
+
+  @org.junit.jupiter.api.Test
+  void size() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void isEmpty() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void top() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void push() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void pop() {
+  }
+}
---
+++ Stacks/src/LinkedStackTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedStackTest {
+
+  @Test
+  void size() {
+  }
+
+  @Test
+  void isEmpty() {
+  }
+
+  @Test
+  void top() {
+  }
+
+  @Test
+  void push() {
+  }
+
+  @Test
+  void pop() {
+  }
+}
